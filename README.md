642372f883e673de706f9d189338b4fd468b5c5b
NordicSnow <zeke@zekial.io>
2020-02-05 12:27:10 -0600

    added imgur integration

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
-# RotorBot v0.6
-a discord bot that replies image links and descriptions. designed to be used for hosting image links and descriptions of user's cars.
-command names, descriptions, and image links are imported from a local JSON file.
+# RotorBot v0.7
+a discord bot that replies image links and descriptions. designed to be used for hosting image links and descriptions of user's cars. command names, descriptions, and image links are imported from a local SQLite database.
 
-Users can also add their own imgur links and descriptions in using the built-in command.
+Users can also upload their own image and description using the built-in command, as well as modify them. Image hosting is done via Imgur's API.
 
-Finally, it does basic functionality like role assigning and welcoming users to the server.
+Finally, it does super basic functionality like role assigning and welcoming users to the server.
--- rotorbot.py
+++ rotorbot.py
@@ -1,5 +1,5 @@
 # libraries
-import os, re, discord, discord.utils, sqlite3, os.path
+import os, re, discord, discord.utils, sqlite3, os.path, requests, json
 from discord.ext import commands
 
 #db connection
@@ -8,11 +8,11 @@ db_path = os.path.join(BASE_DIR, "imagestore.db") #database name configured here
 conn = sqlite3.connect(db_path, isolation_level=None) #set to apply changes on execution
 c = conn.cursor()
 
-#bot token
-token = '<insert token here>' #this should be in a seperate file but its not public so who cares
+#bot tokens
+token = '<insert discord bot token here>' #discord api client token
+clientID='<insert imgur app client ID here>' #imgur api client ID
 
 client = discord.Client() #client object
-defaultChannel = client.get_channel(312216330604642305) #sets default channel as #garage
 
 roleList = ["SA", "FB", "FC", "FD", "RX-8", "MX-5"] #list of roles available to assign via bot
 
@@ -22,9 +22,9 @@ embed = discord.Embed(title="RotorBot Help", colour=discord.Colour(0x29aaca), de
 embed.set_thumbnail(url="https://cdn.discordapp.com/avatars/667799244987695104/a84e8b9d69329358e9a29b4bfeb8b3ca.png?size=256")
 embed.set_author(name="RotorBot", url="https://github.com/NordicSnow/RotorBot", icon_url="https://cdn.discordapp.com/avatars/667799244987695104/a84e8b9d69329358e9a29b4bfeb8b3ca.png?size=256")
 embed.set_footer(text="rotorbot v0.7", icon_url="https://cdn.discordapp.com/avatars/667799244987695104/a84e8b9d69329358e9a29b4bfeb8b3ca.png?size=256")
-embed.add_field(name="Role Picker", value="To pick roles, use the '+addcar' command followed by one of these options:\n***SA***\n***FB***\n***FC***\n***FD***\n***RX-8***\n***MX-5***")
-embed.add_field(name="Image Linker", value="To link a user image, just type in a '+' followed by their username, eg '+nordic'.\nFor a full list of usernames and descriptions, check out http://zekial.io/data.json\nIf you would like yours added, please contact Nordic with a picture and description.")
-embed.add_field(name="Image Assigner", value="To add or edit a user image saved in the image linker, use the command '+addimage [direct imgur link] " + '"[description]"' +"'. Make sure to take note of the quotes on the description, which are required.")
+embed.add_field(name="Role Picker", value="To add a new role, use the '+addcar' command followed by one of these options:\n***SA***\n***FB***\n***FC***\n***FD***\n***RX-8***\n***MX-5***")
+embed.add_field(name="Show an image of a user's car", value="To see a given user's car, just type in a '+' followed by their username, eg '+nordic'.")
+embed.add_field(name="Add yourself to the image linker", value="To add or edit a user image saved in the image linker, use the command '+addimage " + '"[description]"' +"' and attach your image to the message. Make sure to take note of the quotes on the description, which are required.")
 
 #shows console ready message and changes game status
 @client.event
@@ -85,22 +85,26 @@ async def on_message(message):
             await message.channel.send("~uh oh! i've encountered a syntax error! (¤﹏¤)\nremember, "+message.author.name+", the command goes '+addimage [direct imgur link] " + '"[description]"' +"'")
             return #ends add image command to prevent exceptions from occuring due to bad data
         
-        if imgur[:20] != "https://i.imgur.com/": #checks to make sure that user is linking from the correct place, and informs them if not
-            await message.channel.send("~uh oh! i've encountered a syntax error! (¤﹏¤)\nremember, "+message.author.name+", the link must be directly to the image (from 'i.imgur.com', not just 'imgur.com'). Ping Nordic for help!")
+        if message.attachments == []: #checks to make sure that user has included an attachment, and informs them if not
+            await message.channel.send("~uh oh! there isn't an image for me to upload\nremember, "+message.author.name+", the image has to be attached to the invoking message!")
             return #ends add image command to prevent exceptions from occuring due to bad data
 
-        
-        
-        c.execute('SELECT * FROM images WHERE UID =?', uid) #pulls user information
-        currentData = (c.fetchone())
-        if currentData == None: #checks to see if user exists in the database
-            await message.channel.send("hmm, i can't seem to find a record on you. let me create one real quick... ╰(◡‿◡✿╰)") #sends error message
-            c.execute('INSERT INTO images VALUES (?,?,?,?)', (message.author.name.lower(), message.author.id, desc, imgur)) #creates new values using user account and provided information
-            await message.channel.send("~~lovely. i created a record and added in your information! have a nice day! (^▽^)") #sends sucess message
-        else:
-            c.execute('UPDATE images SET description = ?, link = ? WHERE uid = ? ', (desc, imgur, message.author.id)) #updates existing information
-            await message.channel.send("~~thank you!!! ^>^\nyour data has been updated! have a nice day! {◕ ◡ ◕}") #sends confirmation
-        
+
+        response = requests.post('https://api.imgur.com/3/upload', data={'image':message.attachments[0].url}, headers={'Authorization': ('Client-ID ' + clientID)}) #uploads attachment URL to imgur
+        if str(response) == "<Response [200]>": #checks if upload suceeded
+            jsonData = response.json() #reads json data from response
+
+            c.execute('SELECT * FROM images WHERE UID =?', uid) #pulls user information
+            currentData = (c.fetchone())
+            if currentData == None: #checks to see if user exists in the database
+                await message.channel.send("hmm, i can't seem to find a record on you. let me create one real quick... ╰(◡‿◡✿╰)") #sends error message
+                c.execute('INSERT INTO images VALUES (?,?,?,?)', (message.author.name.lower(), message.author.id, desc, jsonData['data']['link'])) #creates new values using user account and provided information
+                await message.channel.send("~~lovely. i created a record and added in your information! have a nice day! (^▽^)") #sends sucess message
+            else:
+                c.execute('UPDATE images SET description = ?, link = ? WHERE uid = ? ', (desc, jsonData['data']['link'], message.author.id)) #updates existing information
+                await message.channel.send("~~thank you!!! ^>^\nyour data has been updated! have a nice day! {◕ ◡ ◕}") #sends confirmation
+        else: #if upload isn't sucessful, throws an error
+            await message.channel.send("Error! Imgur Upload Failed! Either Imgur is down or there is a problem with your image.\nfor debugging: http " + str(response)) #sends confirmation
 
     #help command
     elif message.content.lower() == "+help": #checks if user typed in help command
@@ -109,7 +113,9 @@ async def on_message(message):
 
     elif message.content.lower() == "+iq": #checks if user has indicated they are dealing with a low IQ individual
             await message.channel.send("To be fair, you have to have a very high IQ to drive an FD RX-7.The car is extremely superior to any other modern supercar, and without a solid grasp of theoretical physics you can't even drive it. There's also it’s linear power delivery, which is deftly woven into it’s driving characterisation- it’s personal philosophy draws heavily from Italian designs, for instance. I personally understand this stuff; I have the intellectual capacity to truly appreciate the supreme handling, to realise that it’s not just good- it says something deep about LIFE. As a consequence people who dislike the FD truly ARE idiots- of course they wouldn't appreciate, for instance, the humour in the FD’s existential catchphrase “Boost in, Apex Seals out,” which itself is a cryptic reference to the tenuous balance between life and death. I'm smirking right now just imagining one of those addlepated simpletons scratching their heads in confusion as Yoichi Sato's genius design unfolds itself on the race track. What fools.. how I pity them. :joy: And yes, by the way, i DO have a FD tattoo. And no, you cannot see it. It's for the ladies' eyes only- and even then they have to demonstrate that they're within 5 IQ points of my own (preferably lower) beforehand. Nothin personnel kid :sunglasses:")
-
+    
+    '''elif message.content.lower() == "gapplebees": #checks if user has typed in a stale maymay
+            await message.channel.send("DANGER!!! DANGEROUSLY UNFUNNY MEME DETECTED!!! EXTREME CAUTION ADVISED!!")'''
     #Image Link Handler v0.4 - now ignores case
     firstLetter = message.content[0] #checks if first letter of a message is the invoking one. Can be configured from here.
     if firstLetter == "+": #if not invoking, request is thrown out
